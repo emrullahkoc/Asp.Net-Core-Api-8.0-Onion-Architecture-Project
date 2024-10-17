afe6e769d9abff9169e2b9cedbea5a3f122c2ce8
emrullahkoc <emrullahkoc.1995@gmail.com>
2024-10-17 15:25:06 +0300

    About

--- CarBook/Core/CarBook.Domain/obj/Debug/net8.0/CarBook.Domain.AssemblyInfo.cs
+++ CarBook/Core/CarBook.Domain/obj/Debug/net8.0/CarBook.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc978a7949b2bf3d9304ed130b55cc7321a303be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cda889dbf5610ab70b2b9379811629ddd26032e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarBook.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
